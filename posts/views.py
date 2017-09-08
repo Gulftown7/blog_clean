5e398f6a3c02bd1034ecc988d9908783e19b0667
Gulftown7 <interuros@gmail.com>
2017-09-08 17:11:47 +0200

    minor changes

--- posts/views.py
+++ posts/views.py
@@ -1,22 +1,30 @@
 
 from django.http.response import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 
 
 # Create your views here.
+
+from posts.models import Post
+
+
 def post_create(request):
     return HttpResponse("<h1>Create</h1>")
 
 def post_detail(request):
+    instance = get_object_or_404(Post, id=1)
     context = {
-        "title": "Detail"
+        "instance": instance,
+        "title": instance.title
     }
-    return render(request, "index.html", context )
+    return render(request, "post_detail.html", context )
     
 
 def post_list(request):
+    queryset = Post.objects.all()
     context = {
+        "object_list": queryset,
         "title": "List"
     }
 #     if request.user.is_authenticated():
